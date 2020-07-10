cccf9d6e30d8b161aa1a2ae9ab105a1d9a8c8f99
Kum Somi <54243544+kumsomi@users.noreply.github.com>
2020-07-10 21:17:48 +0530

    Update find_pair_sum.c
    added flag to check for the end condition

--- find_pair_sum.c
+++ find_pair_sum.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 int sum(int a[], int n,int k)
 {
-    
+    int flag=0;
     for(int i=0;i<n-1;i++)
     {
         for(int j=1;j<n;j++)
         if(a[i]+a[j]==k)
+        {
             printf("sum found at index %d and %d\n",i,j);
+            flag++;
+        }
     }
+    if(flag==0)
+        printf("Pairr not found");
     return 0;
 }
 int main()
