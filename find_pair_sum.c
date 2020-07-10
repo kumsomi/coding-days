d6fa63cb9c255225eb358c189d929c7647e29823
Kum Somi <54243544+kumsomi@users.noreply.github.com>
2020-07-10 20:24:17 +0530

    Create find_pair_sum.c
    Given an unsorted array of integers, and we need to find the pair with the given sum.

---
+++ find_pair_sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int sum(int a[], int n,int k)
+{
+    
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=1;j<n;j++)
+        if(a[i]+a[j]==k)
+            printf("sum found at index %d and %d\n",i,j);
+    }
+    return 0;
+}
+int main()
+{
+    int a[10]={1,2,3,4,5,6,7,8,9,10};
+    int k=10;
+    int n=sizeof(a)/sizeof(a[0]);
+    sum(a,n,k);
+    return 0;
+}
